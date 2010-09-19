a570a5f79d9003b1f7961be425dcfdf1599e304e
mik <mik@mik-laptop.(none)>
2010-09-19 20:00:08 +0300

    moved register.html to registration folder renamed registration input mail to email

--- core/views.py
+++ core/views.py
@@ -7,14 +7,14 @@ from django.template.context import RequestContext
 def register(request):
     username = request.POST.get('login','')
     password = request.POST.get('password','')
-    mail = request.POST.get('mail','')
+    mail = request.POST.get('email','')
     if username and password and mail:
         try:
             User.objects.create_user(username, mail, password)
         except:
-            return render_to_response('register.html',{'error':'Username already exist'},context_instance=RequestContext(request))
+            return render_to_response('registration/register.html',{'error':'Username already exist'},context_instance=RequestContext(request))
         return HttpResponseRedirect('/core/login/')
-    return render_to_response('register.html',context_instance=RequestContext(request))
+    return render_to_response('registration/register.html',context_instance=RequestContext(request))
 
 def update_profile():
     pass
