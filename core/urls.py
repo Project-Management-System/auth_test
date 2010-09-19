58dffacd8e71c5b764b68a2fde4ed5ec579fa89a
mik <mik@mik-laptop.(none)>
2010-09-19 19:37:13 +0300

    cleanup code, one url for registration

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,20 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.models import User
 info={'queryset': User.objects.all()}
+
+#profile = {
+#           'template_name':'profile.html'
+#           }
+index = {
+         'template':'index.html'
+         }
 urlpatterns = patterns('',
-    (r'^$','auth_test.core.views.index'),
+    (r'^$','django.views.generic.simple.direct_to_template',index),
     (r'^users/$','django.views.generic.list_detail.object_list',info),
-    (r'^registration/$','django.views.generic.simple.direct_to_template',{'template':'registration.html'}),
     (r'^register/$','auth_test.core.views.register'),
-    (r'^login/$','django.contrib.auth.views.login',{'template_name':'login.html'}),
-    (r'^logout/$','django.contrib.auth.views.logout',{'template_name':'logged_out.html'}),
+    (r'^login/$','django.contrib.auth.views.login'),
+    (r'^logout/$','django.contrib.auth.views.logout',index),
     (r'^user/(?P<object_id>\d+)/$','django.views.generic.list_detail.object_detail',info),
+#    (r'^profile/$','django.views.generic.simple.direct_to_template',profile),
+#    (r'profile/save/$','auth_test.core.views.update_profile'),
 )
--- core/views.py
+++ core/views.py
@@ -2,22 +2,19 @@ from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.contrib.auth import get_user
 from django.shortcuts import render_to_response
-
-def registration(request):
-    user = get_user(request)
-    render_to_response('registration.help',locals())
+from django.template.context import RequestContext
 
 def register(request):
-    username = request.POST['login']
-    password = request.POST['password']
-    mail = request.POST['mail']
-    try:
-        User.objects.create_user(username, mail, password)
-    except:
-        #return render_to_response('registration.html',{'error':'Username already exist'})
-        return HttpResponseRedirect('/core/registration/',{'error':'Username already exist'})
-    return HttpResponseRedirect('/core/users/')
+    username = request.POST.get('login','')
+    password = request.POST.get('password','')
+    mail = request.POST.get('mail','')
+    if username and password and mail:
+        try:
+            User.objects.create_user(username, mail, password)
+        except:
+            return render_to_response('registration.html',{'error':'Username already exist'},context_instance=RequestContext(request))
+        return HttpResponseRedirect('/core/login/')
+    return render_to_response('registration.html',context_instance=RequestContext(request))
 
-def index(request):
-    user = get_user(request)
-    return render_to_response('index.html',locals())
+def update_profile():
+    pass
